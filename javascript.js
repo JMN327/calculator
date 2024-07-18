f28ed14cf00eb6c7b1948eb5dfb0651f8976c155
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-07-18 21:30:00 +0100

    Add 'e' and operator graphics to left of display for UX

--- javascript.js
+++ javascript.js
@@ -2,8 +2,11 @@ let xArray = [];
 let yArray = [];
 let operator;
 let dirty;
-const display = document.querySelector(".display");
+let precision = 11;
+const display = document.querySelector(".display-contents");
 const buttons = document.querySelector(".buttons");
+const eDisplay = document.querySelector(".e");
+const operatorDisplay = document.querySelector(".operator")
 
 initialize();
 updateDisplay(["0"])
@@ -36,27 +39,33 @@ function handleInput(keyText){
       operator === null ? updateNumberArray(keyText, xArray, "x") : updateNumberArray(keyText, yArray, "y") 
       break;
     case "+":
+      operatorDisplay.innerText = "+"
       handleOperator(add)
       break;
     case "-":
     case "−":
+      operatorDisplay.innerText = "−"
       handleOperator(subtract)
       break;
     case "*":
     case "×":
+      operatorDisplay.innerText = "×"
       handleOperator(multiply)
       break;
     case "/":
     case "÷":
+      operatorDisplay.innerText = "÷"
       handleOperator(divide)
       break;
     case "Enter":
     case "=":
+      operatorDisplay.innerText = ""
       evaluate(null);
       break;
     case "Escape":
     case "Delete":
     case "C":
+      operatorDisplay.innerText = ""
       initialize()
       updateDisplay(["0"])
       break;
@@ -68,13 +77,10 @@ function initialize(){
   yArray.length = 0;  //resets the y array
   operator = null;
   dirty = false;
+  eDisplay.innerText = "";
 }
 
 function handleOperator(operatorFunction) {
-  if (!dirty) {
-    yArray.length = 0;
-    operator = null;
-  }
   if (xArray.length === 0) {
     xArray.push("0")
   }
@@ -96,11 +102,25 @@ function evaluate(nextOperator) {
   }
   let x = parseFloat(xArray.join(""));
   let y = parseFloat(yArray.join(""));
-  let result = parseFloat(operator(x,y).toPrecision(11)); //rounds to significant figures but strips zeros
+  let result = parseFloat(operator(x,y).toPrecision(precision + 1)); //rounds to significant figures but strips zeros
   result = String(result).split("")
+  if (result.length > precision) {
+    eDisplay.innerText = "e";
+  } else {
+    eDisplay.innerText = "";
+  }
   updateDisplay(result);
   xArray = result;
-  dirty = false;
+  if (nextOperator === null) {
+    dirty = false;
+    yArray.length = 0;
+    operator = null;
+  } else {
+    operator = nextOperator;
+    yArray.length = 0;
+  }
+  
+  
 }
 
 function updateNumberArray(keyText, thisNumberArray, name) {
@@ -108,11 +128,16 @@ function updateNumberArray(keyText, thisNumberArray, name) {
 
   // process number keys
   if (!isNaN(keyText)) {
-    thisNumberArray.push(keyText);
-    updateDisplay(thisNumberArray, true);
+    operatorDisplay.innerText = "";
+    if(thisNumberArray.length < precision){
+      thisNumberArray.push(keyText);
+      updateDisplay(thisNumberArray, true);
+    }
+
   }
 
   if (keyText === "." && !thisNumberArray.includes(".")) {
+    operatorDisplay.innerText = ""
     if (thisNumberArray.length === 0) {
       thisNumberArray.push("0");
     }
@@ -122,6 +147,7 @@ function updateNumberArray(keyText, thisNumberArray, name) {
 
   if (keyText === "±" || keyText === "F9") {
     if(thisNumberArray.length !== 0) { //check wasn't pressed after operator
+      operatorDisplay.innerText = ""
       if (thisNumberArray[0] === "-") {
         thisNumberArray.shift();
         updateDisplay(thisNumberArray);
@@ -147,7 +173,7 @@ function updateNumberArray(keyText, thisNumberArray, name) {
 }
 
 function updateDisplay(thisNumberArray, isDirty) {
-  let str = thisNumberArray.slice(0,13).join("");
+  let str = thisNumberArray.slice(0, precision).join("");
   console.log("string:" + str);
   display.innerText = str;
   dirty = isDirty ?? dirty;
